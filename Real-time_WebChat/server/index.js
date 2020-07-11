7a2462ef7a35ba7630426d4bc57c1ab94750a37e
Shweta <shweta3047@gmail.com>
2020-07-11 12:39:19 +0530

    make it better

--- Real-time_WebChat/server/index.js
+++ Real-time_WebChat/server/index.js
@@ -1,6 +1,6 @@
 const express =require("express");
 const app=express();
-const PORT=process.env.port||3001
+const PORT=process.env.PORT||3001
 const http=require('http').Server(app);
 const io=require('socket.io')(http);
 const mongoose=require('mongoose');
